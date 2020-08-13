406bc0989623dbae5c797c063f90c52a0db8e3ab
SylarK <syst.lucasamado@gmail.com>
2020-08-13 18:44:47 -0300

    Namespace and keywords

---
+++ Full Stack - Danki Code/3. PHP Orientado_a_Objetos/aula_07/Class1.php
@@ -0,0 +1,14 @@
+<?php
+
+    namespace Sessao1;
+
+    class Class1 {
+
+            function __construct(){
+                new \Class2;
+                echo '<br>Classe instanciada<br>';
+            }   
+
+    }
+
+?>
