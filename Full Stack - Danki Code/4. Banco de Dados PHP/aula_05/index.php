405e44b694c3eec1e02f033e20da4e5a2c8a531c
SylarK <syst.lucasamado@gmail.com>
2020-08-14 18:59:53 -0300

    Inner join, on, as

--- Full Stack - Danki Code/4. Banco de Dados PHP/aula_05/index.php
+++ Full Stack - Danki Code/4. Banco de Dados PHP/aula_05/index.php
@@ -2,31 +2,18 @@
 
     $pdo = new PDO('mysql:host=localhost;port=3306;dbname=modulo_8', 'root', '');
 
+    /*
     $sql = $pdo->prepare("SELECT * FROM posts");
 
     $sql->execute();
 
     $info = $sql->fetchAll();
 
-
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
-</head>
-<body>
-
-    <?php
-
     /*    echo '<pre>';
         print_r($info);
         echo '</pre>';
-    */
-
+    
+    CONSULTA SIMPLES
     foreach ($info as $key => $value) {
         
         echo 'Titulo: '.$value['titulo'];
@@ -34,10 +21,48 @@
         echo 'Texto: '.$value['conteudo'];
         echo '<hr>';
 
-
     }
+    */
 
-    ?>
+    /*
+    $sql = $pdo->prepare("SELECT * FROM categorias");
+    $sql->execute();
+    $info = $sql->fetchAll();
+    */
+    /*
+    FOREACH ENCADEADO PARA BUSCAR A CATEGORIA, E DENTRO DE CADA CATEGORIA OS POSTS
+    foreach ($info as $key => $value) {
+        $categoria_id = $value['id'];
+        echo('Exibindo posts de :'.$value['nome']);
+        echo('<br>');
+        $sql = $pdo->prepare("SELECT * FROM posts WHERE categoria_id = $categoria_id");
+        $sql->execute();
+        $infoPosts = $sql->fetchAll();
+        foreach ($infoPosts as $key => $value) {
+            echo 'Titulo: '.$value['titulo'];
+            echo '<br>';
+            echo 'Texto: '.$value['conteudo'];
+            echo '<hr>';
+        }
+    }
+    */
+
+    $sql =  $pdo->prepare("SELECT * FROM `posts` INNER JOIN `categoria` ON `posts`.`categoria_id`=`categoria`.`id`");
+    $sql->execute();
+
+    /*https://www.php.net/manual/pt_BR/pdostatement.fetch.php*/
+    $info = $sql->fetchAll(PDO::FETCH_ASSOC);
     
-</body>
-</html>
+    echo '<pre>';
+    print_r($info);
+    echo '</pre>';
+
+
+
+
+?>
+
+
+
+    
+
