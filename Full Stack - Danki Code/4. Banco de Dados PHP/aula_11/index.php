52724cb57f7f4e7bab9ae6a4c3d8be4022098800
unknown <lucasamadort@gmail.com>
2020-08-16 19:55:07 -0300

    Finalizando aula sobre database e iniciando novo projeto utilizando o que foi aprendido

---
+++ Full Stack - Danki Code/4. Banco de Dados PHP/aula_11/index.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+    IN BETWEEN   
+    $sql = $pdo->prepare("SELECT * FROM clientes WHERE id IN (1,10) ");
+    $sql = $pdo->prepare("SELECT * FROM clientes WHERE data BETWEEN '2017-01-01' AND '2017-01-20' ");
+
+*/
+
+
+?>
---
+++ Full Stack - Danki Code/5. Projeto I - HTML & CSS/index.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="decription" content="">
+    <meta name="keywords" content="key,words,pso">
+    <title>Web I</title>
+    <link rel="stylesheet" href="style/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet">
+    <link href='style/styles.css' rel='stylesheet'>
+</head>
+<body>
+
+    <header>
+        <div class='center'>
+            <div class="logo">Logo</div>
+            <nav class='desktop'>
+                <ul>    
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Sobre</a></li>
+                    <li><a href='#'>Blog</a></li>
+                    <li><a href='#'>Contato</a></li>
+                </ul>
+            </nav>
+            <nav class='mobile'>
+                <ul>    
+                    <li><a href='#'>Home</a></li>
+                    <li><a href='#'>Sobre</a></li>
+                    <li><a href='#'>Blog</a></li>
+                    <li><a href='#'>Contato</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+
+    <section class='banner-principal'>
+        <div class="center">
+            <form>
+                <h2>Digite o seu email: </h2>
+                <input type='text' name='email'>
+                <input type='submit' name='acao' value='Cadastrar'>
+            </form>
+        </div>
+    </section>
+
+    <section class='quem-somos'>
+        <div class="center">                         
+            <div class='w50'>
+                <h2>Code Steel</h2>
+                <p>Sed scelerisque magna pretium auctor sodales. In et viverra metus, nec fermentum ligula. Phasellus quam sapien, fermentum sit amet ullamcorper 
+                ut, tempus ac nulla. Aliquam imperdiet, lacus vitae posuere faucibus, nulla lectus posuere quam, non laoreet magna risus condimentum est. 
+                Pellentesque purus tortor, cursus eget ante quis, pretium tempor erat. Maecenas dignissim justo at laoreet interdum. 
+                Maecenas pharetra id turpis id venenatis. In sit amet leo id tellus dictum hendrerit in non est. Nullam luctus, dolor pulvinar rhoncus laoreet, 
+                ante justo dignissim massa, et finibus elit purus sed nisi. Sed in risus nulla.</p>
+            </div>
+
+            <div class='w50'>
+                <img src='img/picture.png'>
+            </div>    
+        </div>
+
+    </section>
+
+    <section class='especialidades'>
+        <div class="center">
+            <h2 class='title'></h2>
+            <div class="box-especialidade">
+                <h3>item1</h3>
+                <h3>CSS3</h3>
+                <p>Nam lobortis mi id ligula elementum, non euismod nisl gravida. Nulla ac lectus in dui viverra volutpat at eget leo. 
+                Mauris vel egestas elit. Cras vel augue mi. In purus augue, pulvinar sit amet lobortis eu, rhoncus et ipsum. 
+                Morbi auctor nunc ut ex euismod venenatis. Duis tincidunt ullamcorper ultrices. Pellentesque tempus purus justo, et consequat 
+                orci viverra nec. Curabitur porta feugiat sagittis. Pellentesque a porttitor arcu. Duis molestie, justo at hendrerit vehicula, 
+                est felis congue mauris, in tempus dui purus quis turpis. Phasellus rhoncus magna at urna aliquam, sit amet dignissim lectus fringilla. 
+                Ut ut hendrerit diam. Donec commodo varius malesuada.
+                </p>
+            </div>
+            <div class="box-especialidade">
+                <h3>item2</h3>
+                <h3><i class='fa fa-html5' aria-hidden='true'></i></h3>
+                <p>Nam lobortis mi id ligula elementum, non euismod nisl gravida. Nulla ac lectus in dui viverra volutpat at eget leo. 
+                Mauris vel egestas elit. Cras vel augue mi. In purus augue, pulvinar sit amet lobortis eu, rhoncus et ipsum. 
+                Morbi auctor nunc ut ex euismod venenatis. Duis tincidunt ullamcorper ultrices. Pellentesque tempus purus justo, et consequat 
+                orci viverra nec. Curabitur porta feugiat sagittis. Pellentesque a porttitor arcu. Duis molestie, justo at hendrerit vehicula, 
+                est felis congue mauris, in tempus dui purus quis turpis. Phasellus rhoncus magna at urna aliquam, sit amet dignissim lectus fringilla. 
+                Ut ut hendrerit diam. Donec commodo varius malesuada.
+                </p>
+            </div>
+            <div class="box-especialidade">
+                <h3>item3</h3>
+                <h3>JS</h3>
+                <p>Nam lobortis mi id ligula elementum, non euismod nisl gravida. Nulla ac lectus in dui viverra volutpat at eget leo. 
+                Mauris vel egestas elit. Cras vel augue mi. In purus augue, pulvinar sit amet lobortis eu, rhoncus et ipsum. 
+                Morbi auctor nunc ut ex euismod venenatis. Duis tincidunt ullamcorper ultrices. Pellentesque tempus purus justo, et consequat 
+                orci viverra nec. Curabitur porta feugiat sagittis. Pellentesque a porttitor arcu. Duis molestie, justo at hendrerit vehicula, 
+                est felis congue mauris, in tempus dui purus quis turpis. Phasellus rhoncus magna at urna aliquam, sit amet dignissim lectus fringilla. 
+                Ut ut hendrerit diam. Donec commodo varius malesuada.
+                </p>
+            </div>
+
+        </div>
+    </section>
+    <section class='extras'>
+        <div class="center">
+
+            <div class="w50">
+                <h2 class='title'>Depoimentos</h2>
+                <div class="depoimento-single">
+                    <p class='depoimentos-descricao'>Nam lobortis mi id ligula elementum, non euismod nisl gravida. Nulla ac lectus in dui viverra volutpat at eget leo. 
+                Mauris vel egestas elit. Cras vel augue mi. In purus augue, pulvinar sit amet lobortis eu, rhoncus et ipsum. 
+                Morbi auctor nunc ut ex euismod venenatis. Duis tincidunt ullamcorper ultrices. Pellentesque tempus purus justo, et consequat 
+                orci viverra nec.
+                    </p>
+                    <p class='nome-autor'>Lorem Ipsum</p>
+                </div>
+                <div class="depoimento-single">
+                    <p class='depoimentos-descricao'>Nam lobortis mi id ligula elementum, non euismod nisl gravida. Nulla ac lectus in dui viverra volutpat at eget leo. 
+                Mauris vel egestas elit. Cras vel augue mi. In purus augue, pulvinar sit amet lobortis eu, rhoncus et ipsum. 
+                Morbi auctor nunc ut ex euismod venenatis. Duis tincidunt ullamcorper ultrices. Pellentesque tempus purus justo, et consequat 
+                orci viverra nec.
+                    </p>
+                    <p class='nome-autor'>Lorem Ipsum</p>
+                </div>
+                <div class="depoimento-single">
+                    <p class='depoimentos-descricao'>Nam lobortis mi id ligula elementum, non euismod nisl gravida. Nulla ac lectus in dui viverra volutpat at eget leo. 
+                Mauris vel egestas elit. Cras vel augue mi. In purus augue, pulvinar sit amet lobortis eu, rhoncus et ipsum. 
+                Morbi auctor nunc ut ex euismod venenatis. Duis tincidunt ullamcorper ultrices. Pellentesque tempus purus justo, et consequat 
+                orci viverra nec.
+                    </p>
+                    <p class='nome-autor'>Lorem Ipsum</p>
+                </div>
+            </div>
+            <div class="w50">
+                <h2 class='title'>Serviços</h2>
+                <div class="servicos">
+                    <ul>
+                        <li>
+                        Nam lobortis mi id ligula elementum, non euismod nisl gravida. Nulla ac lectus in dui viverra volutpat at eget leo. 
+                        Mauris vel egestas elit. Cras vel augue mi. In purus augue, pulvinar sit amet lobortis eu, rhoncus et ipsum. 
+                        </li>
+                        <li>
+                        Nam lobortis mi id ligula elementum, non euismod nisl gravida. Nulla ac lectus in dui viverra volutpat at eget leo. 
+                        Mauris vel egestas elit. Cras vel augue mi. In purus augue, pulvinar sit amet lobortis eu, rhoncus et ipsum. 
+                        </li>
+                        <li>
+                        Nam lobortis mi id ligula elementum, non euismod nisl gravida. Nulla ac lectus in dui viverra volutpat at eget leo. 
+                        Mauris vel egestas elit. Cras vel augue mi. In purus augue, pulvinar sit amet lobortis eu, rhoncus et ipsum. 
+                        </li>
+                        
+                    </ul>
+                
+                </div>
+            </div>
+        
+        </div>
+    </section>
+
+    <footer>
+
+        <div class="center">
+            <p>Todos os direitos reservados.</p>
+        </div>
+
+    </footer>
+
+    
+</body>
+</html>
